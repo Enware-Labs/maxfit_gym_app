2c42ae3abc1c84ef4135be5f0ee9810837a5c021
gimnakatugampala <gimnakatugampala1@gmail.com>
2022-01-16 22:59:42 +0530

    home screen

--- screens/Home/HomeScreen.js
+++ screens/Home/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 export default function HomeScreen() {
     return (
@@ -19,9 +19,14 @@ export const HomeScreenOptions = (navData) =>({
     // headerRight: () => (
        
     // ),
-    // headerLeft:() => (
-       
-    // )
+    headerLeft:() => (
+        <Image
+        style={{width:50,height:50,marginLeft:5}}
+        source={{
+          uri: 'https://scontent.fcmb7-1.fna.fbcdn.net/v/t1.6435-9/208859872_203169565143798_8794676154000761999_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGzS0RYIPqaGquaNIWACrE20_Q2S3nENy_T9DZLecQ3L9hmojHDul6EJPoCKFilG9psKSVI0nKQljOlfPGS2v2h&_nc_ohc=anlW6cSFcEEAX-OdN0Z&tn=CvZTsjQbeLrMYJCw&_nc_ht=scontent.fcmb7-1.fna&oh=00_AT_-8hrX6Dm_cBrYzBT6ixvsU86B2kD_zZFws28M_A33IQ&oe=620B58BF',
+        }}
+      />
+    )
   })
 
 const styles = StyleSheet.create({})
